72c45d4c444e0495e9a53f2331e416b4b8430997
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 19:54:33 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,50 +1,55 @@
-def run(songs)
-  help
-  while true do
-    puts "Please enter a command:"
-    response = gets.chomp
-    case response
-    when "exit"
-      exit_jukebox
-      break
-    when "play"
-      play(songs)
-    when "help"
-      help
-    when "list"
-      list(songs)
-    else
-      puts "Invalid entry"
-    end
+# Add your code here
+
+def help
+  puts  "I accept the following commands:"
+  puts  "- help : displays this help message"
+  puts  "- list : displays a list of songs you can play"
+  puts  "- play : lets you choose a song to play"
+  puts  "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
   end
 end
 
 def play(songs)
   puts "Please enter a song name or number:"
   response = gets.chomp
-  if response.to_i >= 1 && response.to_i <= songs.length
-    puts "Playing #{songs[response.to_i-1]}"
-  elsif songs.include?(response)
-    puts "Playing #{songs.find{|song| song == response}}"
-  else
-    puts "Invalid input, please try again"
+
+  solution = songs.detect do |song|
+    response == song
   end
+
+  if !solution
+    songs.length > response ? solution = "Invalid input, please try again" : solution = response - 1
+  end
+
+  puts solution
 end
 
 def exit_jukebox
   puts "Goodbye"
 end
 
-def help
-puts  "I accept the following commands:"
-puts  "- help : displays this help message"
-puts  "- list : displays a list of songs you can play"
-puts  "- play : lets you choose a song to play"
-puts  "- exit : exits this program"
-end
-
-def list(songs)
-songs.each_with_index {|song, index|
-  puts "#{index+1}. #{song}"
-}
+def run(songs)
+  help
+  while true do
+    puts "Please enter a command:"
+    input = gets.strip
+    case input
+    when "exit"
+      exit_jukebox
+      break
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "help"
+      help
+    else
+      puts "Invalid input, please try again"
+    end
+  end
 end
